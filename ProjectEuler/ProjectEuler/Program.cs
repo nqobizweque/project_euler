3a92426292d834dcf1d3fb19af06e54cc5ecc1d6
Q <s214008231@nmmu.ac.za>
2019-05-20 17:01:24 +0200

    First Commit

---
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProjectEuler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            Display(nameof(Solutions.Solution1), Solutions.Solution1(1000));
+            Display(nameof(Solutions.Solution2), Solutions.Solution2(4000000));
+            Console.ReadLine();
+        }
+
+        static void Display<T>(string name, T result)
+        {
+            Console.WriteLine($"{name}: {result}");
+        }
+    }
+}
---
+++ ProjectEuler/ProjectEuler/Solutions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public static class Solutions
+    {
+        public static int Solution1(int max)
+        {
+            int sum = 0, num = 3;
+            while (num < max)
+            {
+                sum += (num % 3 == 0 || num % 5 == 0 ? num : 0);
+                num += 1;
+            }
+            return sum;
+        }
+
+        public static int Solution2(int max)
+        {
+            int sum = 2, first = 1, second = 2;
+            while (first < max && second < max)
+            {
+                var temp = first + second;
+                first = second; second = temp;
+                sum += (second % 2 == 0 ? second : 0);
+            }
+            return sum;
+        }
+
+        public static int Solution3(int num)
+        {
+            int prime = int.MinValue;
+            num = num / 2 + 1;
+            while(num > 0)
+            {
+
+                
+            }
+            return prime;
+        }
+    }
+}
